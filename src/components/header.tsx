c34f011058e3ac3c76ea5636c2c431be64a0f2be
guicodee <guilhermedfgo12@gmail.com>
2024-09-10 17:34:18 -0300

    feat: add auth handler to header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,17 @@
+'use client'
+
 import {
   House,
   ListCollapse,
   LogIn,
+  LogOut,
   MenuIcon,
   Percent,
   ShoppingBag,
   ShoppingCart
 } from "lucide-react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Card } from "./ui/card";
@@ -20,6 +25,15 @@ import {
 } from "./ui/sheet";
 
 export default function Header() {
+  const { data, status } = useSession()
+
+  async function handleSignIn() {
+    await signIn('google')
+  }
+
+  async function handleSignOut() {
+    await signOut()
+  }
 
   return (
     <Card className="px-7 py-4 flex justify-between items-center">
@@ -35,13 +49,38 @@ export default function Header() {
             <SheetTitle className="mb-4">Menu</SheetTitle>
           </SheetHeader>
 
+          {status === "authenticated" && data.user && (
+            <div className="flex items-center gap-2 mb-6">
+              <Image 
+                src={data.user.image!}
+                alt={data.user.name!}
+                width={40}
+                height={40}
+                className="rounded-full"
+              />
+              
+              <div >
+                <span className="text-zinc-200 text-sm">{data.user.name}</span>
+                <h1 className="text-zinc-400 text-xs">{data?.user?.email}</h1>
+              </div>
+            </div>
+          )}
+
           <Separator />
 
           <div className="mt-4 flex flex-col gap-3">
-            <Button variant={"outline"} className="w-full justify-start gap-2">
-              <LogIn size={18} />
-              Fazer login
-            </Button>
+
+            {status === "unauthenticated" ? (
+              <Button variant={"outline"} className="w-full justify-start gap-2" onClick={handleSignIn}>
+                <LogIn size={18} />
+                Fazer login
+              </Button>
+            ) : (
+              <Button variant={"outline"} className="w-full justify-start gap-2" onClick={handleSignOut}>
+                <LogOut size={18} />
+                Desconectar
+              </Button>
+            )}
             <Button variant={"outline"} className="w-full justify-start gap-2">
               <House size={18} />
               Início
