b316204188513609b683c45e02ca3e629ef1777d
guicodee <guilhermedfgo12@gmail.com>
2024-09-11 17:04:26 -0300

    feat: add banner  and listing of categories to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,31 @@
-export default function Home() {
+import Image from "next/image";
+import CategoryList from "./components/category-list";
+
+export default async function Home() {
+  
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="space-y-12">
+      <Image 
+        width={0}
+        height={0}
+        src={'/banner-ofertas.png'}
+        className="w-full mx-auto max-lg:hidden"
+        sizes="100vw"
+        alt="Até 55% de desconto nesse mês."
+      />
+
+      <div className="mx-auto flex flex-col px-5 gap-8 lg:container lg:gap-10">
+        <Image 
+          width={0}
+          height={0}
+          src={'/banner-descount.png'}
+          sizes="100vw"
+          className="h-auto w-full lg:hidden"
+          alt="Até 55% de desconto nesse mês."
+        />
+        
+        <CategoryList />
+      </div>
     </div>
   );
 }
