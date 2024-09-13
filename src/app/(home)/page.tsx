8d3ad2c2248c423d07569ac0fb58de9c292a6d28
guicodee <guilhermedfgo12@gmail.com>
2024-09-13 14:56:47 -0300

    feat: add keyboards and banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import ProductList from "@/components/ui/product-list";
 import { prismaClient } from "@/lib/prisma";
 import Image from "next/image";
+import Link from "next/link";
 import CategoryList from "./components/category-list";
 
 export default async function Home() {
@@ -8,6 +9,14 @@ export default async function Home() {
     where: {
       discountPercentage: {
         gt: 0,
+      }
+    }
+  })
+
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: 'keyboards'
       }
     }
   })
@@ -36,8 +45,46 @@ export default async function Home() {
         <CategoryList />
       </div>
 
-      <div className="px-2">
-        <ProductList products={productsDiscount} />
+      <div className="px-2 space-y-4">
+        <ProductList products={productsDiscount} category="Ofertas" />
+      </div>
+
+      <div className="flex items-center px-5 gap-5">
+        <Link href={'/'}>
+          <Image 
+            src={'/banner-mouses.png'}
+            alt="Até 55% de desconto em mouses."
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </Link>
+        <Link href={'/'}>
+          <Image 
+            src={'/banner-fones.png'}
+            alt="Até 55% de desconto em fones."
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </Link>
+      </div>
+
+      <div className="px-2 space-y-4">
+        <ProductList products={keyboards} category="Teclados" />
+      </div>
+
+      <div className="flex items-center px-5 gap-5">
+        <Image 
+          src={'/banner-fretegratis.png'}
+          alt="Frete grátis para todo o Brasil."
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
       </div>
     </div>
   );
--- src/components/ui/product-list.tsx
+++ src/components/ui/product-list.tsx
@@ -1,18 +1,28 @@
 import { computeProductTotalPrice } from "@/helpers/product"
 import { Product } from "@prisma/client"
+import { ArrowRight } from "lucide-react"
+import Link from "next/link"
 import ProductItem from "./product-item"
 
 interface ProductListProps {
   products: Product[]
+  category: string
 }
 
-export default function ProductList({ products }: ProductListProps) {
+export default function ProductList({ products, category }: ProductListProps) {
 
   return (
-    <div className="flex w-full gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
-      {products.map(( product ) => (
-        <ProductItem key={product.id} product={computeProductTotalPrice(product)} />
-      ))}
+    <div className="flex flex-col w-full gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+      <Link href={'/'} className="flex items-center gap-2 px-4">
+        <h1 className="font-black text-lg uppercase hover:underline">{category}</h1>
+        <ArrowRight size={18} />
+      </Link>
+
+      <div className="flex items-center gap-4">
+        {products.map(( product ) => (
+          <ProductItem key={product.id} product={computeProductTotalPrice(product)} />
+        ))}
+      </div>
     </div>
   )
 }
