afbdc14807b681fd160afdae30efa7233c8b62c8
guicodee <guilhermedfgo12@gmail.com>
2024-09-13 15:50:01 -0300

    feat: add footer component

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+
+  return (
+    <div className="px-20 py-6 bg-accent mt-16">
+      <p className="text-sm text-zinc-300 font-semibold">© 2024 <span className="font-black text-zinc-200">NexBuy</span></p>
+    </div>
+  )
+}
