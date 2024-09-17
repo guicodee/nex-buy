85033a1044d7fa71011f7b0aa3ed38e8e979052b
guicodee <guilhermedfgo12@gmail.com>
2024-09-17 14:54:53 -0300

    feat: add total, subtotal and total discount calculations to cart context

--- src/providers/cart-provider.tsx
+++ src/providers/cart-provider.tsx
@@ -2,7 +2,7 @@
 
 import { getLocalStorageCartProducts } from '@/helpers/get-local-storage';
 import { ProductWithTotalPrice } from '@/helpers/product';
-import { createContext, ReactNode, useEffect, useState } from 'react';
+import { createContext, ReactNode, useEffect, useMemo, useState } from 'react';
 
 export interface CartProduct extends ProductWithTotalPrice {
 	quantity: number;
@@ -10,6 +10,12 @@ export interface CartProduct extends ProductWithTotalPrice {
 
 interface ICartContext {
 	products: CartProduct[];
+	cartTotalPrice: number;
+	cartBasePrice: number;
+	cartTotalDiscount: number;
+	subtotal: number;
+	total: number;
+	totalDiscount: number;
 	addProdcutsToCart: (product: CartProduct) => void;
 	decreaseProductQuantity: (productId: string) => void;
 	increaseProductQuantity: (productId: string) => void;
@@ -18,6 +24,12 @@ interface ICartContext {
 
 export const CartContext = createContext<ICartContext>({
 	products: [],
+	cartTotalPrice: 0,
+	cartBasePrice: 0,
+	cartTotalDiscount: 0,
+	subtotal: 0,
+	total: 0,
+	totalDiscount: 0,
 	addProdcutsToCart: () => {},
 	decreaseProductQuantity: () => {},
 	increaseProductQuantity: () => {},
@@ -31,6 +43,20 @@ export default function CartProvider({ children }: { children: ReactNode }) {
 	useEffect(() => {
 		localStorage.setItem('@cart-products/products', JSON.stringify(products));
 	}, [products]);
+
+	const subtotal = useMemo(() => {
+		return products.reduce((acc, product) => {
+			return acc + Number(product.basePrice) * product.quantity;
+		}, 0);
+	}, [products]);
+
+	const total = useMemo(() => {
+		return products.reduce((acc, product) => {
+			return acc + product.totalPrice * product.quantity;
+		}, 0);
+	}, [products]);
+
+	const totalDiscount = subtotal - total;
 
 	function addProdcutsToCart(product: CartProduct) {
 		const productIsAlreadyOnCart = products.some(
@@ -95,10 +121,16 @@ export default function CartProvider({ children }: { children: ReactNode }) {
 		<CartContext.Provider
 			value={{
 				products,
+				subtotal,
+				total,
+				totalDiscount,
 				addProdcutsToCart,
 				decreaseProductQuantity,
 				increaseProductQuantity,
 				removeProductToCart,
+				cartBasePrice: 0,
+				cartTotalDiscount: 0,
+				cartTotalPrice: 0,
 			}}
 		>
 			{children}
