ec9587498ef72a2787f43958978e80e55f6a24f6
guicodee <guilhermedfgo12@gmail.com>
2024-09-21 17:38:11 -0300

    feat: add orders page

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,44 @@
+import { Badge } from '@/components/ui/badge';
+import { authOptions } from '@/lib/auth';
+import { prismaClient } from '@/lib/prisma';
+import { ShoppingBasket } from 'lucide-react';
+import { getServerSession } from 'next-auth';
+import OrderItem from './components/order-item';
+
+export default async function Orders() {
+	const user = await getServerSession(authOptions);
+
+	if (!user?.user) return <h1>Acesso negado</h1>;
+
+	const orders = await prismaClient.order.findMany({
+		where: {
+			userId: user.user.id,
+			status: 'PAYMENT_CONFIRMED',
+		},
+		include: {
+			orderProducts: {
+				include: {
+					product: true,
+				},
+			},
+		},
+	});
+
+	return (
+		<div className="container mx-auto py-4 px-4 space-y-12">
+			<Badge
+				variant={'outline'}
+				className="px-4 py-2 flex items-center gap-3 w-fit uppercase font-bold text-base border-violet-400"
+			>
+				<ShoppingBasket size={18} />
+				Meus pedidos
+			</Badge>
+
+			<div>
+				{orders.map((order) => (
+					<OrderItem key={order.id} order={order} />
+				))}
+			</div>
+		</div>
+	);
+}
