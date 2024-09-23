41a11143b72363eadb7116ba484dc9e4c11ebe6a
guicodee <guilhermedfgo12@gmail.com>
2024-09-23 19:11:53 -0300

    feat: add informations on end valores of products purchased

--- src/app/orders/components/order-item.tsx
+++ src/app/orders/components/order-item.tsx
@@ -6,8 +6,10 @@ import {
 } from '@/components/ui/accordion';
 import { Card } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
+import { computeProductTotalPrice } from '@/helpers/product';
 import { Prisma } from '@prisma/client';
 import { format } from 'date-fns';
+import { useMemo } from 'react';
 import OrderProductItem from './order-product-item';
 
 interface OrderItemProps {
@@ -23,6 +25,21 @@ interface OrderItemProps {
 }
 
 export default function OrderItem({ order }: OrderItemProps) {
+	const subtotal = useMemo(() => {
+		return order.orderProducts.reduce((acc, product) => {
+			return acc + Number(product.basePrice) * product.quantity;
+		}, 0);
+	}, [order.orderProducts]);
+
+	const total = useMemo(() => {
+		return order.orderProducts.reduce((acc, product) => {
+			const ProductWithTotalPrice = computeProductTotalPrice(product.product);
+			return acc + Number(ProductWithTotalPrice.totalPrice) * product.quantity;
+		}, 0);
+	}, [order.orderProducts]);
+
+	const totalDiscount = total - subtotal;
+
 	return (
 		<Card>
 			<Accordion type="single" collapsible className="w-full px-5">
@@ -62,6 +79,33 @@ export default function OrderItem({ order }: OrderItemProps) {
 									orderProduct={orderProduct}
 								/>
 							))}
+						</div>
+
+						<div className="flex flex-col space-y-4">
+							<Separator className="mt-4" />
+
+							<div className="flex text-sm text-zinc-200">
+								<p className="flex-1">Subtotal</p>
+								<p>R$ {subtotal.toFixed(2)}</p>
+							</div>
+
+							<Separator />
+							<div className="flex text-sm text-zinc-200">
+								<p className="flex-1">Entrega</p>
+								<p>GRÁTIS</p>
+							</div>
+
+							<Separator />
+							<div className="flex text-sm text-zinc-200">
+								<p className="flex-1">Descontos</p>
+								<p>-R$ {totalDiscount.toFixed(2)}</p>
+							</div>
+
+							<Separator />
+							<div className="flex text-sm font-bold text-zinc-200">
+								<p className="flex-1">Total</p>
+								<p>R$ {total.toFixed(2)}</p>
+							</div>
 						</div>
 					</AccordionContent>
 				</AccordionItem>
