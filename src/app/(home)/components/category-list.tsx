186e1db73a492c7520a495392afa8767466e5c95
guicodee <guilhermedfgo12@gmail.com>
2024-09-24 13:45:31 -0300

    fix: add to link in categories of home page

--- src/app/(home)/components/category-list.tsx
+++ src/app/(home)/components/category-list.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button';
 import { CATEGORY_ICON } from '@/constants/category-icon';
 import { prismaClient } from '@/lib/prisma';
+import Link from 'next/link';
 
 export default async function CategoryList() {
 	const categories = await prismaClient.category.findMany({});
@@ -8,12 +9,14 @@ export default async function CategoryList() {
 	return (
 		<div className="flex items-center justify-center gap-6 max-lg:grid max-lg:grid-cols-2">
 			{categories.map((category) => (
-				<Button key={category.id} variant={'outline'}>
-					<div className="flex items-center px-4 gap-2">
-						{CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
-						<span>{category.name}</span>
-					</div>
-				</Button>
+				<Link href={`/category/${category.slug}`}>
+					<Button key={category.id} variant={'outline'}>
+						<div className="flex items-center px-4 gap-2">
+							{CATEGORY_ICON[category.slug as keyof typeof CATEGORY_ICON]}
+							<span>{category.name}</span>
+						</div>
+					</Button>
+				</Link>
 			))}
 		</div>
 	);
